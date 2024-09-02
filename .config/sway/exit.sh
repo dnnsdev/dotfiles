f273e651b0bae550141f62f8290c6855c81b8616
Dennis V <me@dnns.dev>
2024-09-02 22:02:44 +0200

    Removed obsolete stuff and clear up scripts

--- .config/sway/exit.sh
+++ .config/sway/exit.sh
@@ -12,7 +12,4 @@ if [[ ! $(pgrep -x "swaynag") ]]; then
         --border=#FCC99D \
         #--text=#092E47 \
         --background=#282828
-    #swaynag --background 333333 --border 333333 --border-bottom 333333 --button-background 1F1F1F \
-	#    --button-border-size 0 --button-padding 8 --text FFFFFF --button-text FFFFFF --edge bottom \
-	#    -t warning -m 'Do you really want to log out?' -B 'You heard me!' 'swaymsg exit'
 fi
