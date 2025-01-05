1c0eda5c8bcbd50a0d0349c391379beb9bbea8bf
Dennis V <me@dnns.dev>
2025-01-05 19:56:56 +0100

    Optimized setup_environment.sh - now check for existing files

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -24,66 +24,100 @@ apt-get install ca-certificates curl wget gpg apt-transport-https nala
 
 # < Docker
 
-apt-get install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+if [ $(which docker) == "" ];
+then
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
 
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-groupadd docker
-usermod -aG docker $username
+  apt-get update
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  groupadd docker
+  usermod -aG docker $username
+
+fi
 
 # > Docker
 
 # < (vs)code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+if [ $(which code) == "" ];
+then
 
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+  install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
+fi
 # > (vs)code
 
 # < fastfetch
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.27.1/fastfetch-linux-amd64.deb
-dpkg -i fastfetch-linux-amd64.deb
+if [ $(which fastfetch) == "" ];
+then
+
+  wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.27.1/fastfetch-linux-amd64.deb
+  dpkg -i fastfetch-linux-amd64.deb
+
+fi
 
 # > fastfetch
 
 # < spotify
 
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+if [ $(which spotify) == "" ];
+then
+
+  curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+  echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+
+fi
 
 # > spotify
 
 # < owncloud-client
 
-wget -nv https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Debian_12/Release.key -O - | gpg --dearmor | tee /etc/apt/trusted.gpg.d/owncloud-client.gpg > /dev/null
+if [ $(which owncloud) == "" ];
+then
 
-echo 'deb https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Debian_12/ /' | tee -a /etc/apt/sources.list.d/owncloud-client.list
+  wget -nv https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Debian_12/Release.key -O - | gpg --dearmor | tee /etc/apt/trusted.gpg.d/owncloud-client.gpg > /dev/null
+
+  echo 'deb https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Debian_12/ /' | tee -a /etc/apt/sources.list.d/owncloud-client.list
+
+fi
 
 # > owncloud-client
 
 # source: https://github.com/jellyfin/jellyfin-media-player/wiki/Installing-on-Linux
 
-version=$(curl --head https://github.com/jellyfin/jellyfin-media-player/releases/latest | tr -d '\r' | grep '^location' | sed 's/.*\/v//g')
-wget "https://github.com/jellyfin/jellyfin-media-player/releases/download/v$version/jellyfin-media-player_$version-1_amd64-$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2).deb" -O jmp.deb
+if [ $(which docker) == "" ];
+then
 
-apt install ./jmp.deb
+  version=$(curl --head https://github.com/jellyfin/jellyfin-media-player/releases/latest | tr -d '\r' | grep '^location' | sed 's/.*\/v//g')
+  wget "https://github.com/jellyfin/jellyfin-media-player/releases/download/v$version/jellyfin-media-player_$version-1_amd64-$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2).deb" -O jmp.deb
+
+  apt install ./jmp.deb
+
+fi
 
 # > owncloud client
 
 # > starship
 
-curl -sS https://starship.rs/install.sh | sh
+if [ $(which starship) == "" ];
+then
+
+  curl -sS https://starship.rs/install.sh | sh
+
+fi
 
 # < starship
 
@@ -117,37 +151,57 @@ fi
 
 # > fonts
 
-# < thorium
-wget -O ./thorium-browser.deb "$(curl -s https://api.github.com/repos/Alex313031/Thorium/releases/latest | grep browser_download_url | grep .deb | cut -d '"' -f 4)"
+# < thorium-browser
+if [ $(which thorium-browser) == "" ];
+then
 
-nala install ./thorium-browser.deb -y
+  wget -O ./thorium-browser.deb "$(curl -s https://api.github.com/repos/Alex313031/Thorium/releases/latest | grep browser_download_url | grep .deb | cut -d '"' -f 4)"
+
+  nala install ./thorium-browser.deb -y
+
+fi
 
 # > thorium
 
 # < dotnet sdk
 
-wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+if [ $(which dotnet) == "" ];
+then
 
-dpkg -i packages-microsoft-prod.deb
+  wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+
+  dpkg -i packages-microsoft-prod.deb
+
+fi
 
 # > dotnet sdk
 
 # < vesktop
 
-wget https://vencord.dev/download/vesktop/amd64/deb -O vesktop.deb
+if [ $(which vesktop) == "" ];
+then
 
-dpkg -i vesktop.deb
+  wget https://vencord.dev/download/vesktop/amd64/deb -O vesktop.deb
+
+  dpkg -i vesktop.deb
+
+fi
 
 # > vesktop
 
 # < VSCodium
 
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+if [ $(which codium) == "" ];
+then
 
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | tee /etc/apt/sources.list.d/vscodium.list
+  wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+      | gpg --dearmor \
+      | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+  echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+      | tee /etc/apt/sources.list.d/vscodium.list
+
+fi
 
 # > VSCodium
 
