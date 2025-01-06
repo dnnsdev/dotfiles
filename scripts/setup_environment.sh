6abb043d6cf610cad09a3acdcb6b7cc2483a5244
Dennis V <me@dnns.dev>
2025-01-06 21:45:23 +0100

    Update fastfetch to latest version

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -64,7 +64,7 @@ fi
 if [ $(which fastfetch) == "" ];
 then
 
-  wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.27.1/fastfetch-linux-amd64.deb
+  wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.33.0/fastfetch-linux-amd64.deb
   dpkg -i fastfetch-linux-amd64.deb
 
 fi
