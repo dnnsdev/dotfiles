ae7e88642249fbf60f71ba421f43b1e37b7eb1a7
Dennis <dennis@t.sch27>
2025-01-22 15:50:42 +0100

    Dont need anything with pacman since im on debian

--- .config/sway/conf.d/startup.sh
+++ .config/sway/conf.d/startup.sh
@@ -7,3 +7,9 @@ for app in "${apps[@]}"
 do
     swaymsg "exec --no-startup-id $app"
 done
+
+#virsh net-define /usr/share/libvirt/networks/default.xml
+
+#virsh net-autostart default
+
+#virsh net-start default
