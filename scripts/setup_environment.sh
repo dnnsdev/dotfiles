b7e240ccc1c4781f5e9af58f73aba0530127151a
Dennis <dennis@t.sch27>
2025-02-04 15:02:18 +0100

    Update spotify key

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -76,7 +76,8 @@ fi
 if [ $(which spotify) == "" ];
 then
 
-  curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+  curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+  
   echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 
 fi
