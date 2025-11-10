8f42152bc1acf6f74cb589560795a78a52d29d8b
Dennis <me@dnns.dev>
2025-11-10 16:46:26 +0100

    * Script was checking for wrong binary.

--- scripts/setup/spotify.sh
+++ scripts/setup/spotify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if ! command -v spotify-client >/dev/null 2>&1; then
+if ! command -v spotify >/dev/null 2>&1; then
   curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb https://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 else
-  echo "[INFO] spotify-client is already installed."
+  echo "[INFO] spotify is already installed."
 fi
