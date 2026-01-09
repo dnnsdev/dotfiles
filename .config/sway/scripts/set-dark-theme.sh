f30db4935d0e12c679be89b61d2c28dbf419e283
Dennis <me@dnns.dev>
2026-01-09 13:29:17 +0100

    * Remove obsolete files

--- .config/sway/scripts/set-dark-theme.sh
+++ .config/sway/scripts/set-dark-theme.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Set environment variables for dark theme
-export GTK_THEME=Aretha-Dark-GTK:dark
-export QT_STYLE_OVERRIDE=adwaita-dark  
-export GTK_APPLICATION_PREFER_DARK_THEME=1
-export gtk-application-prefer-dark-theme=1
+# Ensure GTK can find themes in user directory
+export GTK_DATA_HOME="$HOME/.local/share"
+export GTK_THEME_DIR="$HOME/.themes"
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
-# Update systemd user environment
-systemctl --user set-environment GTK_THEME="$GTK_THEME"
-systemctl --user set-environment QT_STYLE_OVERRIDE="$QT_STYLE_OVERRIDE"
-systemctl --user set-environment GTK_APPLICATION_PREFER_DARK_THEME="$GTK_APPLICATION_PREFER_DARK_THEME"
-
-# Set gsettings
+# Set GTK theme settings
 gsettings set org.gnome.desktop.interface gtk-theme 'Aretha-Dark-GTK'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+
+# Set environment variables for current session
+export GTK_THEME='Aretha-Dark-GTK'
+export QT_STYLE_OVERRIDE=adwaita-dark
+export GTK_APPLICATION_PREFER_DARK_THEME=1
+
+# Update systemd user environment
+systemctl --user import-environment \
+    GTK_DATA_HOME \
+    GTK_THEME_DIR \
+    GTK_THEME \
+    QT_STYLE_OVERRIDE \
+    GTK_APPLICATION_PREFER_DARK_THEME
 
 # Update dbus activation environment
 dbus-update-activation-environment --systemd \
     WAYLAND_DISPLAY \
     XDG_CURRENT_DESKTOP \
+    GTK_DATA_HOME \
+    GTK_THEME_DIR \
     GTK_THEME \
     QT_STYLE_OVERRIDE \
     GTK_APPLICATION_PREFER_DARK_THEME
