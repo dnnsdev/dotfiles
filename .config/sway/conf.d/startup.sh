f7540ee11fe164040b959bd30d77ec32de156277
Dennis <me@dnns.dev>
2026-01-11 22:33:25 +0100

    Optimized startup

--- .config/sway/conf.d/startup.sh
+++ .config/sway/conf.d/startup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+
+# Wait for sway/network stuff
+sleep 2
+
+# Mount filesystems
+mount -a
+
 apps=( owncloud vesktop swaync rancher-desktop keepassxc )
 
-for app in "${apps[@]}"
-do
-    swaymsg "exec --no-startup-id $app"
+for app in "${apps[@]}"; do
+    if command -v "$app" &> /dev/null; then
+        swaymsg "exec --no-startup-id $app" || logger "Failed to launch $app"
+        sleep 0.5
+    else
+        logger "Warning: $app not found in PATH"
+    fi
 done
-
-#virsh net-define /usr/share/libvirt/networks/default.xml
-
-#virsh net-autostart default
-
-#virsh net-start default
